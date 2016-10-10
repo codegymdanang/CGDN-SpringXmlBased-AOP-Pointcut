f43329036c0f47b80d5af00d68f9e2e9e03a2c0d
nguyenle <nguyen.levu@smartdev.vn>
2016-10-10 17:19:16 +0700

    commit

---
+++ sprintaoppointcut/src/main/java/com/hvcntt/sprintpointcut/model/Employee.java
@@ -0,0 +1,19 @@
+package com.hvcntt.sprintpointcut.model;
+
+public class Employee {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void throwException() {
+		throw new RuntimeException("Dummy");
+	}
+	
+}
---
+++ sprintaoppointcut/src/main/java/com/hvcntt/sprintpointcut/service/EmployeeService.java
@@ -0,0 +1,5 @@
+package com.hvcntt.sprintpointcut.service;
+
+public class EmployeeService {
+
+}
